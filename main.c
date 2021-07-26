86cd10397b7c93dad0b17068e5862986e7831702
a.maslov <maslov@lcard.ru>
2021-07-26 15:23:14 +0300

    Uart debug

--- main.c
+++ main.c
@@ -11,15 +11,8 @@ static volatile bool run;
 Timer timer2;
 Usart usart0;
 
-void traceOutchar(char c)
-{
-    usart_putchar(&usart0, c);
-}
-
-void traceFlush(void)
-{
-    usart_flush(&usart0);
-}
+uint8_t usart0Data[6];
+char usart0DataStr[128];
 
 void ignite(MeterSpark spark) {
     //TODO: Ignite
@@ -27,16 +20,25 @@ void ignite(MeterSpark spark) {
 
 int main(void)
 {
-  cli();
-  run = true;
-  sei();
-  usart_init(&usart0, USART_0, DEBUG_BAUDRATE);
-  usart_putstr(&usart0, "System ready");
-  //timer_configSimple(&timer2, TIMER_2, WD_RESET_FREQ_HZ, NULL, TIMER_OUTPUT_TOGGLE_B);
-  //meter_init(ignite);
+    cli();
+    run = true;
+    sei();
+    usart_init(&usart0, USART_0, DEBUG_BAUDRATE);
+    usart0Data[0] = UCSR0A;
+    usart0Data[1] = UCSR0B;
+    usart0Data[2] = UCSR0C;
+    usart0Data[3] = UBRR0L;
+    usart0Data[4] = UBRR0H;
+    sprintf(usart0DataStr, "UCSR0A = 0x%02X; UCSR0B = 0x%02X; UCSR0C = 0x%02X; UBRR0L = 0x%02X; UBRR0H = 0x%02X;",
+            usart0Data[0], usart0Data[1], usart0Data[2], usart0Data[3], usart0Data[4]);
+    UDR0='!';
+    usart_putstr(&usart0, usart0DataStr);
+    usart_putstr(&usart0, "System ready");
+    //timer_configSimple(&timer2, TIMER_2, WD_RESET_FREQ_HZ, NULL, TIMER_OUTPUT_TOGGLE_B);
+    //meter_init(ignite);
 
-  while(!run);
-  while(run) {
-  }
-  return 0;
+    while(!run);
+    while(run) {
+    }
+    return 0;
 }
